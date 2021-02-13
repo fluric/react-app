f3a7fff4b49677b89413c12011bb550aa7191897
chris <christian.fluri@hotmail.com>
2021-02-13 21:26:28 +0100

    [no-issue] Add greedy player

--- src/main/webapp/components/RockPaperScissorsStrategies.js
+++ src/main/webapp/components/RockPaperScissorsStrategies.js
@@ -1,14 +1,30 @@
-import { random } from 'lodash';
+import { last, random } from 'lodash';
 
-import { ActionList } from 'main/webapp/utilities/RockPaperScissorsUtility';
+import { ActionList, Actions } from 'main/webapp/utilities/RockPaperScissorsUtility';
 
 const getDummyAction = () => {
   return ActionList[0];
 };
 
-export const getOpponentAction = () => {
+export const getGreedyAction = actionHistory => {
+  if (actionHistory.length === 0) {
+    return getRandomAction();
+  }
+  const lastAction = last(actionHistory).action;
+
+  switch (lastAction) {
+    case Actions.SCISSORS:
+      return Actions.ROCK;
+    case Actions.ROCK:
+      return Actions.PAPER;
+    case Actions.PAPER:
+      return Actions.SCISSORS;
+  }
+};
+
+export const getRandomAction = () => {
   const index = random(2);
   return ActionList[index];
 };
 
-export const Strategies = { 1: getDummyAction, 2: getOpponentAction };
+export const Strategies = { 1: getDummyAction, 2: getGreedyAction, 3: getRandomAction };
