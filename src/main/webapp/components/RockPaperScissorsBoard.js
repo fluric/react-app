5c4abed62d75ba910ac0b13b1e780895128ff5c9
chris <christian.fluri@hotmail.com>
2021-02-13 19:01:22 +0100

    [no-issue] Add level display

--- src/main/webapp/components/RockPaperScissorsBoard.js
+++ src/main/webapp/components/RockPaperScissorsBoard.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
-import { Grid, GridRow, Segment } from 'semantic-ui-react';
+import { Grid, GridRow, Header, Segment } from 'semantic-ui-react';
 import { RockPaperScissors } from 'main/webapp/components/RockPaperScissors';
 import { Outcomes } from 'main/webapp/utilities/RockPaperScissorsUtility';
+import { FormattedMessage } from 'react-intl';
 
-const ScoreBoard = ({ score, opponentScore }) => <Segment compact>{`${score} : ${opponentScore}`}</Segment>;
+const ScoreBoard = ({ score, opponentScore }) => (
+  <Segment compact>
+    <FormattedMessage id={'app.scoreBoard.result'} values={{ score, opponentScore }} />
+  </Segment>
+);
+
+const LevelDisplay = () => (
+  <Header size={'medium'} textAlign={'center'}>
+    <FormattedMessage id={'app.scoreBoard.level'} values={{ level: '1' }} />
+  </Header>
+);
 
 export const RockPaperScissorsBoard = () => {
   const [score, setScore] = useState(0);
@@ -16,6 +27,9 @@ export const RockPaperScissorsBoard = () => {
 
   return (
     <Grid centered columns={1}>
+      <GridRow>
+        <LevelDisplay />
+      </GridRow>
       <GridRow>
         <ScoreBoard score={score} opponentScore={opponentScore} />
       </GridRow>
