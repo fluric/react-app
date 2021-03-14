50235783d62a468b482f0b9c7ebdacd070787fad
chris <christian.fluri@hotmail.com>
2021-03-14 09:13:27 +0100

    [no-issue] Test HashRouter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
 
 import store from 'main/webapp/store';
 import App from 'main/webapp/App';
 import messages from 'main/webapp/translations/en.json';
-import { BASE_NAME } from 'main/webapp/Routes';
 
 ReactDOM.render(
-  <BrowserRouter basename={BASE_NAME}>
+  <HashRouter>
     <Provider store={store}>
       <IntlProvider locale={'en'} messages={messages}>
         <App />
       </IntlProvider>
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
