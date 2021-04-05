aa3324c51b9babaa8d66cce9e18a473f468538b4
chris <christian.fluri@hotmail.com>
2021-04-05 18:23:11 +0200

    [no-issue] Add getBalancedAction method

--- src/main/webapp/components/rock-paper-scissors/RockPaperScissorsConfig.js
+++ src/main/webapp/components/rock-paper-scissors/RockPaperScissorsConfig.js
@@ -1,15 +1,19 @@
 import {
+  getBalancedAction,
   getDummyAction,
+  getEverChangingAction,
   getGreedyAction,
   getLastWinAction,
   getNoScissorsAction,
   getTit42TatAction,
 } from 'main/webapp/components/rock-paper-scissors/RockPaperScissorsStrategies';
 import bargrak from 'main/webapp/images/bargrakIronFist.jpg';
-import gothmog from 'main/webapp/images/gothmogTheDetermined.jpg';
+import gothmog from 'main/webapp/images/gothmogTheUnbalanced.jpg';
 import golum from 'main/webapp/images/greedyGolum.jpg';
 import saruman from 'main/webapp/images/cunningSaruman.jpg';
-import shokk from 'main/webapp/images/shokkTheReluctant.jpg';
+import denethor from 'main/webapp/images/denethorTheUnstable.png';
+import shokk from 'main/webapp/images/shokkTheDetermined.jpg';
+import smaug from 'main/webapp/images/smaugMasterOfSkies.jpeg';
 
 export const opponents = {
   1: {
@@ -20,33 +24,33 @@ export const opponents = {
   2: {
     strategy: getLastWinAction,
     image: shokk,
-    name: 'Shokk the Reluctant',
+    name: 'Shokk the Determined',
   },
-  // 3: {
-  //   strategy:, //TODO: always changing
-  //   image: denethor,
-  //   name: 'Denethor the Unstable'
-  // }
   3: {
-    strategy: getNoScissorsAction,
-    image: gothmog,
-    name: 'Gothmog the Determined',
+    strategy: getEverChangingAction,
+    image: denethor,
+    name: 'Denethor the Unstable',
   },
   4: {
+    strategy: getNoScissorsAction,
+    image: gothmog,
+    name: 'Gothmog the Unbalanced',
+  },
+  5: {
     strategy: getGreedyAction,
     image: golum,
     name: 'Greedy Golum',
   },
-  5: {
+  6: {
     strategy: getTit42TatAction,
     image: saruman,
     name: 'Cunning Saruman',
   },
-  // 7: {
-  //   strategy: , //TODO: balanced play, always play what was chosen the least often, otherwise random
-  //   image: smaug,
-  //   name: 'Smaug Master of Skies',
-  // },
+  7: {
+    strategy: getBalancedAction,
+    image: smaug,
+    name: 'Smaug Master of the Skies',
+  },
   // 8: {
   //   strategy: , //TODO: play the move that it lost the longest ago
   //   image: shelob,
--- src/main/webapp/components/rock-paper-scissors/RockPaperScissorsStrategies.js
+++ src/main/webapp/components/rock-paper-scissors/RockPaperScissorsStrategies.js
@@ -1,7 +1,8 @@
-import { last, nth, random } from 'lodash';
+import { countBy, last, nth, random, sample } from 'lodash';
 
 import {
   ActionList,
+  Actions,
   getOutcome,
   getWinningAction,
   Outcomes,
@@ -29,11 +30,34 @@ export const getLastWinAction = actionHistory => {
   return getRandomAction();
 };
 
+export const getEverChangingAction = actionHistory => {
+  const randomAction = getRandomAction();
+
+  if (actionHistory.length === 0) {
+    return randomAction;
+  }
+
+  const lastAction = last(actionHistory).opponentAction;
+  if (randomAction === lastAction) {
+    return getEverChangingAction(actionHistory);
+  }
+  return randomAction;
+};
+
 export const getTit42TatAction = actionHistory => {
   if (actionHistory.length < 2) {
     return getRandomAction();
   }
   return nth(actionHistory, -2).action;
+};
+
+export const getBalancedAction = actionHistory => {
+  const initValues = { [Actions.ROCK]: 0, [Actions.SCISSORS]: 0, [Actions.PAPER]: 0 };
+  const countedActions = { ...initValues, ...countBy(actionHistory, 'opponentAction') };
+  const minimum = Math.min(...Object.values(countedActions));
+
+  const minimumActions = Object.keys(countedActions).filter(key => countedActions[key] === minimum);
+  return sample(minimumActions);
 };
 
 export const getNoScissorsAction = () => {
